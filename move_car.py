54c3d053bd70340ce5eb58260adb68b9a9f9fedc
deniswillett <StevenWillett@Stevens-iMac-5.local>
2017-02-23 17:41:10 -0500

    More group comments...

--- move_car.py
+++ move_car.py
@@ -1,5 +1,6 @@
 # Descriptive Header
 
+#Global variables defined out of class
 directions = ['N','E','S','W'] 
 movement = {'N': (0,1), 'E': (1,0), 'S': (0,-1), 'W':(-1,0)}
 commands = {'L': 'turn_left', 'R': 'turn_right', 'M': 'move'}
@@ -23,7 +24,7 @@ class Vehicle():
     def turn_right(self):
         self.dir = directions[(directions.index(self.dir)+1)%len(directions)]
 
-    def move(self):
+    def move(self): #does not handle linear movement?
         new_x = self.x + movement[self.dir][0]
         new_y = self.y + movement[self.dir][1]
 
@@ -38,6 +39,7 @@ vehicle_one_commands = raw_input()
 
 vehicle_one = Vehicle(int(vehicle_one_pos[0]), int(vehicle_one_pos[1]), vehicle_one_pos[2])
 for command in vehicle_one_commands:
+    # Maybe better to have function here, not eval class
     eval("vehicle_one.{0}()".format(commands[command]))
 
 first_vehicle_x = vehicle_one.x
@@ -54,6 +56,7 @@ and some examples may be useful...
 vehicle_two_pos = raw_input().split()
 vehicle_two_commands = raw_input()
 
+# typo - pos
 vehicle_two = Vehicle(int(vehicle_two_pos[0]), int(vehicle_two_pos[1]), vehicle_two_ps[2])
 for command in vehicle_two_commands:
     eval("vehicle_two.{0}()".format(commands[command]))
